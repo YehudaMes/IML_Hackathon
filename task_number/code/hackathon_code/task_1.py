76317d557f76217d1dbb7b2b2826dfb74a8bcaab
Tehila.Mescheloff <tehila.mescheloff@sisense.com>
2023-06-08 19:16:00 +0300

    edit some code

--- task_number/code/hackathon_code/task_1.py
+++ task_number/code/hackathon_code/task_1.py
@@ -17,7 +17,7 @@ from sklearn.ensemble import GradientBoostingClassifier, VotingClassifier, Rando
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.decomposition import PCA
 
-from task_number.code.hackathon_code.utils import save_model
+from task_number.code.hackathon_code.utils import save_model, load_model
 
 models = [
     # ("Nearest Neighbors", KNeighborsClassifier(5)),
@@ -143,20 +143,24 @@ def ensemble_classification_model(X: np.ndarray, y: np.ndarray) -> BaseEstimator
     # Train and evaluate models
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Define the individual models
-    model1 = LogisticRegression(max_iter=1000)
-    model2 = RandomForestClassifier()
-    model3 = SVC()
-
-    # Create the ensemble by combining the models using VotingClassifier
-    ensemble = VotingClassifier(estimators=[('lr', model1), ('rf', model2), ('svc', model3)], voting='hard')
-
-    # Train the ensemble model
-    ensemble.fit(X_train, y_train)
-
+    ensemble = fit_and_save_ensemble_3('splitted_', X_train, y_train)
     # Make predictions
     y_pred = ensemble.predict(X_test)
     print("ensemble accuracy: ", accuracy_score(y_test, y_pred))
+    return ensemble
+
+
+def fit_and_save_ensemble_3(X_train, y_train, prefix_name):
+    print(prefix_name+'ensemble_3')
+    # Define the individual models
+    model1 = DecisionTreeClassifier(max_depth=5)
+    model2 = AdaBoostClassifier()
+    model3 = GradientBoostingClassifier()
+    # Create the ensemble by combining the models using VotingClassifier
+    ensemble = VotingClassifier(estimators=[('dt', model1), ('ab', model2), ('gbc', model3)], voting='hard')
+    # Train the ensemble model
+    ensemble.fit(X_train, y_train)
+    save_model(ensemble, prefix_name+'ensemble_3')
     return ensemble
 
 
@@ -193,39 +197,32 @@ def kernel_methods_classification(X: np.ndarray, y: np.ndarray):
     return best_model
 
 
-def search_best_hyperparameters(X: np.ndarray, y: np.ndarray):
-    # Split the data into training and testing sets
+def load_model_and_predict(X: np.ndarray, y: np.ndarray):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    results = []
+    for name, _ in models.copy():
+        model = load_model(name)
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        print(f"{name} scores: \naccuracy: {accuracy},\nprecision: {precision},\nrecall: {recall},\nf1: {f1}\n\n")
+        results.append((name, accuracy, precision, recall, f1))
 
-    # Define the parameter grid for GridSearchCV
-    param_grid = {
-        'C': [0.1, 1, 10],
-        'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
-        'gamma': ['scale', 'auto']
-    }
+        # Create a DataFrame to store the results
+    df_results = pd.DataFrame(results, columns=['Model', 'Accuracy', 'Precision', 'Recall', 'F1-score'])
 
-    # Create the SVM model
-    model = SVC()
+    best_model = find_best_model(df_results)
 
-    # Create the GridSearchCV object
-    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5)
+    # Print the best model
+    print(f"The best model is: {best_model}")
 
-    # Fit the GridSearchCV object to the training data
-    grid_search.fit(X_train, y_train)
+    # Plot the results
+    plot_4_measures(df_results, 'Performance of Different Classification Models')
 
-    # Get the best parameters and best score
-    best_params = grid_search.best_params_
-    best_score = grid_search.best_score_
+    return best_model
 
-    # Use the best model to make predictions
-    best_model = grid_search.best_estimator_
-    y_pred = best_model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-
-    # Print the best parameters, best score, and accuracy
-    print('Best Parameters:', best_params)
-    print('Best Score:', best_score)
-    print('Accuracy:', accuracy)
 
 def fit_and_save_model(X: np.ndarray, y: np.ndarray):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -246,5 +243,5 @@ if __name__ == "__main__":
     # kernel_methods_classifcation(X,y)
     # search_best_hyperparameters(X, y)
     # evaluate_different_models(data, y)
-
-    # fit_and_save_model(X, y)
+    fit_and_save_ensemble_3(X, y, "full_train_")
+    # ensemble_classification_model(X, y)
