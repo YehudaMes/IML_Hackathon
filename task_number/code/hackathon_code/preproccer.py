fe21104ff085f5e4af18ccf87009be134baed195
Itai Hajaj <itaihjj@cs.huji.ac.il>
2023-06-08 10:23:16 +0300

    Start Of Hackathon!

---
+++ task_number/code/hackathon_code/preproccer.py
@@ -0,0 +1,4 @@
+
+
+
+data = pd.read_csv(DATA_PATH)
--- task_number/code/hackathon_code/train_validation_partition.py
+++ task_number/code/hackathon_code/train_validation_partition.py
@@ -1,29 +1,26 @@
-import os
-
-import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 DATA_PATH = "../../../Agoda - Data/agoda_cancellation_train.csv"
-TRAIN_PATH = "./data/train/"
-VALIDATION_PATH = "./data/validation/"
+TRAIN_PATH = "data/Train.cvs"
+VALIDATION_PATH = "data/Validation.cvs"
+RANDOM_SEED = 0
+TEST_SIZE = 0.2
 
 
-def create_data_directories():
-    os.makedirs(TRAIN_PATH, exist_ok=True)
-    os.makedirs(VALIDATION_PATH, exist_ok=True)
+def split_data():
+    # Read the CSV file into a pandas DataFrame
+    data = pd.read_csv(DATA_PATH)
+
+    # Split the data into training and test sets
+    train_data, test_data = train_test_split(data, test_size=TEST_SIZE, random_state=RANDOM_SEED)
+
+    # Save the training set to a CSV file
+    train_data.to_csv(TRAIN_PATH, index=False)
+
+    # Save the test set to a CSV file
+    test_data.to_csv(VALIDATION_PATH, index=False)
 
 
-def validation_indexes(data_len, validation_len):
-    np.random.seed(26)
-    return np.random.choice(np.arange(data_len), validation_len)
-
-
-def split_save_data():
-    df = pd.read_csv(DATA_PATH)
-    validation_inds = validation_indexes(len(df), len(df) // 4)
-    validation_set = df[validation_inds]
-    validation_set.to_csv(VALIDATION_PATH + "validation.csv")
-    df.drop(validation_inds).to_csv(VALIDATION_PATH + "train.csv")
-
-if __name__ == '__main__':
-    split_save_data()
+if __name__ == "__main__":
+    split_data()
