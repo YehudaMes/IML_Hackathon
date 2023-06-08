6cca1d1025db80e292013ee9e9589e7ca9b3c1f3
Itai Hajaj <itaihjj@cs.huji.ac.il>
2023-06-08 10:27:31 +0300

    Start Of Hackathon!

--- task_number/code/hackathon_code/preproccer.py
+++ task_number/code/hackathon_code/preproccer.py
@@ -1,4 +1,11 @@
+import pandas as pd
+
+TRAIN_PATH = "/data/train.cvs"
+# VALIDATION_PATH = "/data/train.cvs"
+
+data = pd.read_csv(TRAIN_PATH)
 
 
+def hey:
+    pass
 
-data = pd.read_csv(DATA_PATH)
