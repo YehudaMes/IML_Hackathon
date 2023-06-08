e46aff85684eecbe4a506d325401f8504f76b94b
Tehila.Mescheloff <tehila.mescheloff@sisense.com>
2023-06-09 01:02:14 +0300

    some changes

--- task_number/code/hackathon_code/task_1.py
+++ task_number/code/hackathon_code/task_1.py
@@ -7,7 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis, LinearDiscriminantAnalysis
 import pandas as pd
 from sklearn.base import BaseEstimator
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
@@ -20,21 +20,33 @@ from sklearn.decomposition import PCA
 from utils import save_model, load_model
 
 models = [
-    # ("Nearest Neighbors", KNeighborsClassifier(5)),
-    # ("Linear SVM", SVC(kernel="linear", probability=True)),
+    ("Nearest Neighbors", KNeighborsClassifier(5)),
+    ("LDA", LinearDiscriminantAnalysis(store_covariance=True)),
     # ("RBF SVM", SVC(gamma=2, C=1)),
     # ("Gaussian Process", GaussianProcessClassifier(1.0 * RBF(1.0))),
-    ("Decision Tree", DecisionTreeClassifier(max_depth=5)),
-    # ("Random Forest 5", RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1)),
-    # ("Neural Net", MLPClassifier(alpha=1, max_iter=1000)),
+    ("Decision Tree 10", DecisionTreeClassifier(max_depth=10)),
+    ("Decision Tree 5", DecisionTreeClassifier(max_depth=5)),
+    ("Random Forest 5", RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1)),
     ("AdaBoost", AdaBoostClassifier()),
     # ("Naive Bayes", GaussianNB()),
     # ("QDA", QuadraticDiscriminantAnalysis()),
     ('Logistic Regression', LogisticRegression(max_iter=1000)),
     ('Generic Random Forest', RandomForestClassifier()),
     # ('Support Vector Machines', SVC()),
-    ('Gradient Boosting', GradientBoostingClassifier())
+    ('Gradient Boosting', GradientBoostingClassifier()),
+    ("Decision Tree", DecisionTreeClassifier()),
+
+    ("Neural Net", MLPClassifier(alpha=1, max_iter=1000)),
+    ("Default Logistic Regression", LogisticRegression()),
+    ("Linear SVM", SVC(kernel="linear"))
+
 ]
+
+# models = [
+#
+#     ("Decision Tree 1", DecisionTreeClassifier(max_depth=5))
+#
+# ]
 
 
 def find_best_model(df_results):
@@ -60,14 +72,11 @@ def plot_results(df_results, title):
     fig.show()
 
 
-def plot_4_measures(df_results, title):
+def plot_2_measures(df_results, title):
     fig = go.Figure()
     fig.add_traces(data=[
         go.Scatter(mode="markers", marker=dict(size=10), name='Accuracy', x=df_results['Model'],
                    y=df_results['Accuracy']),
-        go.Scatter(mode="markers", marker=dict(size=10), name='Precision', x=df_results['Model'],
-                   y=df_results['Precision']),
-        go.Scatter(mode="markers", marker=dict(size=10), name='Recall', x=df_results['Model'], y=df_results['Recall']),
         go.Scatter(mode="markers", marker=dict(size=10), name='F1-score', x=df_results['Model'],
                    y=df_results['F1-score'])
     ])
@@ -79,36 +88,7 @@ def plot_4_measures(df_results, title):
     )
     # Show the plot
     fig.show()
-
-
-def choose_classification_model(X: np.ndarray, y: np.ndarray)-> BaseEstimator:
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Train and evaluate models
-    results = []
-    for name, model in models.copy():
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        precision = precision_score(y_test, y_pred)
-        recall = recall_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred)
-        print(f"{name} scores: \naccuracy: {accuracy},\nprecision: {precision},\nrecall: {recall},\nf1: {f1}\n\n")
-        results.append((name, accuracy, precision, recall, f1))
-
-    # Create a DataFrame to store the results
-    df_results = pd.DataFrame(results, columns=['Model', 'Accuracy', 'Precision', 'Recall', 'F1-score'])
-
-    best_model = find_best_model(df_results)
-
-    # Print the best model
-    print(f"The best model is: {best_model}")
-
-    # Plot the results
-    plot_4_measures(df_results, 'Performance of Different Classification Models')
-
-    return best_model
+    return fig
 
 
 def choose_cross_validation_classification_model(X: np.ndarray, y: np.ndarray) -> BaseEstimator:
@@ -165,41 +145,7 @@ def fit_and_save_ensemble_3(X_train, y_train, prefix_name):
     return ensemble
 
 
-def kernel_methods_classification(X: np.ndarray, y: np.ndarray):
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Perform PCA for dimensionality reduction
-    pca = PCA(n_components=2)  # Adjust the number of components as desired
-    X_train_pca = pca.fit_transform(X_train)
-    X_test_pca = pca.transform(X_test)
-
-    # Define the kernel methods to try
-    kernel_methods = ['linear', 'poly', 'rbf', 'sigmoid']
-
-    # Train and evaluate SVM models with different kernel methods
-    results = []
-    for kernel in kernel_methods:
-        model = SVC(kernel=kernel)
-        model.fit(X_train_pca, y_train)
-        y_pred = model.predict(X_test_pca)
-        accuracy = accuracy_score(y_test, y_pred)
-        f1 = f1_score(y_test,y_pred)
-        results.append((kernel, accuracy, f1))
-
-    # Create a DataFrame to store the results
-    df_results = pd.DataFrame(results, columns=['Kernel Method', 'Accuracy', 'F1-score'])
-
-    best_model = find_best_model(df_results)
-    # Print the best model
-    print(f"The best model is: {best_model}")
-    plot_results(df_results, 'Performance of kernel-methods SVC')
-
-    return best_model
-
-
-def load_model_and_predict(X: np.ndarray, y: np.ndarray):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def load_model_and_predict(X_test: np.ndarray, y_test: np.ndarray):
     results = []
     for name, _ in models.copy():
         model = load_model(name)
@@ -207,7 +153,7 @@ def load_model_and_predict(X: np.ndarray, y: np.ndarray):
         accuracy = accuracy_score(y_test, y_pred)
         precision = precision_score(y_test, y_pred)
         recall = recall_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred, average='macro')
         print(f"{name} scores: \naccuracy: {accuracy},\nprecision: {precision},\nrecall: {recall},\nf1: {f1}\n\n")
         results.append((name, accuracy, precision, recall, f1))
 
@@ -220,13 +166,12 @@ def load_model_and_predict(X: np.ndarray, y: np.ndarray):
     print(f"The best model is: {best_model}")
 
     # Plot the results
-    plot_4_measures(df_results, 'Performance of Different Classification Models')
+    plot_2_measures(df_results, 'Performance of Different Classification Models')
 
     return best_model
 
 
-def fit_and_save_model(X: np.ndarray, y: np.ndarray):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def fit_and_save_model(X_train: np.ndarray, y_train: np.ndarray):
     for name, model in models.copy():
         print(f"saving {name}")
         model.fit(X_train, y_train)
@@ -234,15 +179,20 @@ def fit_and_save_model(X: np.ndarray, y: np.ndarray):
 
 
 if __name__ == "__main__":
-    from preproccer_task1 import load_data
-    df = load_data()
-    X, y = df.loc[:, df.columns != "cancellation_indicator"], df.cancellation_indicator
-    # plot_data(data,y)
-    # choose_classification_model(X, y)
-    choose_cross_validation_classification_model(X, y)
-    # ensemble_classification_model(X, y)
-    # kernel_methods_classifcation(X,y)
-    # search_best_hyperparameters(X, y)
-    # evaluate_different_models(data, y)
-    fit_and_save_ensemble_3(X, y, "full_train_")
-    # ensemble_classification_model(X, y)
+
+    from preproccer_task1 import load_train_data_task1
+    X, y = load_train_data_task1()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    #
+    # # plot_data(data,y)
+    # # choose_classification_model(X, y)
+    # # choose_cross_validation_classification_model(X, y)
+    # # ensemble_classification_model(X, y)
+    # # kernel_methods_classifcation(X,y)
+    # # search_best_hyperparameters(X, y)
+    # # evaluate_different_models(data, y)
+    fit_and_save_model(X_train, y_train)
+    load_model_and_predict(X_test, y_test)
+    # fit_and_save_ensemble_3(X, y, "full_agoda_")
+    # # ensemble_classification_model(X, y)
+    # plot_train_result().write_image("classification_models_performance.png")
