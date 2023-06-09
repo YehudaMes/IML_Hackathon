5f67b5bbadc9d4b07f61d695f0dfa5ff99ef5b64
Itai Hajaj <itaihjj@cs.huji.ac.il>
2023-06-09 03:53:16 +0300

    Start Of Hackathon!

--- README.txt
+++ README.txt
@@ -1,1 +1,11 @@
-# IML_Hackathon
+agoda_data - a file containing 3 csv files for train and test
+
+columns_data - contains a list of the columns created in the training stage to ve used to recreate a 
+df with the corresponding columns in the test stage
+
+data- contains a split of the original training data giving into train and validation data
+
+trained_model - contains our fitted models
+
+hackathon_code - contains all the python files used fo preprocessing and training
+
--- task_number/code/hackathon_code/preproccer_task1_not_ready.py
+++ task_number/code/hackathon_code/preproccer_task1_not_ready.py
@@ -3,93 +3,125 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-# DATA_PATH = "../../../agoda_data/agoda_cancellation_train.csv" // todo: at end this one should be used!
-DATA_PATH = "data/train.csv"
+COLUMNS_DATA_PATH = './hackathon_code/columns_data/task1_columns.txt'
+
+MEANS = {'hotel_star_rating': 3,
+         'no_of_adults': 2,
+         'no_of_extra_bed': 0,
+         'no_of_room': 1,
+         'no_of_children': 0,
+         'days_before_cancelled': 10,
+         'original_selling_amount': 219,
+         "booking_to_checkin": 35}
 
 COLS_TO_DROP = ["h_booking_id", "hotel_id", "cancellation_datetime", "checkin_date", "checkout_date",
                 "hotel_brand_code", "hotel_chain_code", "hotel_live_date", "booking_datetime",
                 "request_nonesmoke", "request_latecheckin", "request_highfloor", "request_largebed",
-                "request_twinbeds", "request_airport", "request_earlycheckin", "customer_nationality",
-                "h_customer_id", "no_of_children", "no_of_adults", "no_of_extra_bed", "no_of_room",
+                "request_twinbeds", "request_airport", "request_earlycheckin",
+                # "customer_nationality",
+                "h_customer_id",
+                # "no_of_children", "no_of_adults",
+                # "no_of_extra_bed", "no_of_room",
                 "origin_country_code", "hotel_city_code", "is_user_logged_in", "original_payment_currency",
                 "original_payment_method", "guest_is_not_the_customer", "language"]
-
-pd.set_option('display.max_rows', None)
 
 COLUMNS_TO_DUMMIES = [
     "accommadation_type_name", "charge_option", "guest_nationality_country_name",
     "hotel_country_code", "hotel_area_code", "is_first_booking", "cancellation_policy_code",
-    "original_payment_type"
-]
+    "original_payment_type", "customer_nationality"]
 
 
-# Classification
-def load_data():
-    df = pd.read_csv(DATA_PATH)
+def booking_to_checkin_feature(df):
+    df["checkin_date"] = pd.to_datetime(df["checkin_date"])
+    df["booking_to_checkin"] = pd.to_datetime(df["checkin_date"]) - pd.to_datetime(df["booking_datetime"])
+    df["booking_to_checkin"] = df["booking_to_checkin"].fillna(pd.Timedelta(0)).dt.days.astype(int)
+
+
+def preprocess_train_task1(path, with_dummies):
+    df = pd.read_csv(path)
 
     df["cancellation_indicator"] = df["cancellation_datetime"].notnull().astype(int)  # Task 1 labeling
-
-    produce_days_before_cancelling_feature(df)  # todo Takes care on negative days
-
+    booking_to_checkin_feature(df)
     df = df.drop(COLS_TO_DROP, axis=1)
     df = df[df['hotel_star_rating'].isin(np.arange(0, 5.5, 0.5))]
+    df = df[df['booking_to_checkin'].isin(np.arange(0, 350, 1))]
 
-    counts = df.groupby('is_first_booking')['cancellation_indicator'].apply(
-        lambda x: pd.Series({'nan_count': x.isna().sum(), 'not_nan_count': x.notna().sum()}))
+    if with_dummies:
+        df = pd.get_dummies(df, columns=COLUMNS_TO_DUMMIES)
 
-    # todo dummies: "accommadation_type_name", "charge_option", "guest_nationality_country_name",
-    #   "hotel_country_code", "hotel_area_code", "is_first_booking", "cancellation_policy_code",
-    #    "original_payment_type"
+    # Save the column names as a text file
+    with open(COLUMNS_DATA_PATH, 'w') as file:
+        file.write('\n'.join(df.columns))
 
-    # x = df['guest_is_not_the_customer', 'cancellation_indicator'].groupby('guest_is_not_the_customer')
-    # grouped_counts = df.groupby('guest_is_not_the_customer')['cancellation_indicator'].value_counts()
-    # print(grouped_counts)
+    X, y = df.drop('cancellation_indicator', axis=1), df['cancellation_indicator']
 
+    return X, y
+
+
+def preprocess_data_to_validation_task1(path):
+    df = pd.read_csv(path)
+
+    # Read columns feature of Trained model
+    with open(COLUMNS_DATA_PATH, 'r') as file:
+        desired_columns = file.read().splitlines()
+
+    df["cancellation_indicator"] = df["cancellation_datetime"].notnull().astype(int)  # Task 1 labeling
+    booking_to_checkin_feature(df)
+
+    df.loc[(~df['hotel_star_rating'].isin(np.arange(0, 5, 0.5))), 'hotel_star_rating'] = MEANS['hotel_star_rating']
+    df.loc[(df['original_selling_amount'] < 10) | (df['original_selling_amount'] > 5000),
+           'original_selling_amount'] = MEANS['original_selling_amount']
+    df.loc[(df['booking_to_checkin'] < 0) | (df['booking_to_checkin'] > 350),
+           'booking_to_checkin'] = MEANS['booking_to_checkin']
+
+    df = df.drop(COLS_TO_DROP, axis=1)
+    df = pd.get_dummies(df, columns=COLUMNS_TO_DUMMIES)
+    df = df.reindex(columns=desired_columns, fill_value=0)
+
+    X, y = df.drop('cancellation_indicator', axis=1), df['cancellation_indicator']
+    return X, y
+
+
+def preprocess_predict_task1(path):
+    df = pd.read_csv(path)
+
+    # Read columns feature of Trained model
+    with open(COLUMNS_DATA_PATH, 'r') as file:
+        desired_columns = file.read().splitlines()
+
+    booking_to_checkin_feature(df)
+    df.loc[(~df['hotel_star_rating'].isin(np.arange(0, 5, 0.5))), 'hotel_star_rating'] = MEANS['hotel_star_rating']
+    df.loc[(df['original_selling_amount'] < 10) | (df['original_selling_amount'] > 5000),
+           'original_selling_amount'] = MEANS['original_selling_amount']
+    df.loc[(df['booking_to_checkin'] < 0) | (df['booking_to_checkin'] > 350),
+           'booking_to_checkin'] = MEANS['booking_to_checkin']
+
+    cols_to_drop = COLS_TO_DROP.copy()
+    cols_to_drop.remove("cancellation_datetime")
+
+    df = df.drop(cols_to_drop, axis=1)
     df = pd.get_dummies(df, columns=COLUMNS_TO_DUMMIES)
 
+    desired_columns.remove('cancellation_indicator')
+    df = df.reindex(columns=desired_columns, fill_value=0)
     return df
-    print(df.columns.size)
-    # print(df.dtypes)
-
-    # for col in ['sqft_lot', 'sqft_living', 'price']:
-    #     df = df[df[col] > 0]
-    #
-    # for col in ['sqft_basement', 'sqft_above', 'yr_renovated']:
-    #     df = df[df[col] >= 0]
-    #
-    # df = df[df['sqft_lot'] < 2500000]
-    # df = df[df['sqft_living'] < 11000]
-    # df = df[df['sqft_above'] < 8000]
-    # df = df[df['sqft_basement'] < 2750]
-    # df = df[df['price'] < 7100000]
-    # df = df[(df['yr_renovated'] == 0) | ((df['yr_renovated'] >= 1900) & (df['yr_renovated'] <= 2015))]
-    #
-    # df = df[df['condition'].isin(range(1, 6)) &
-    #         df['yr_built'].isin(range(1900, 2016)) &
-    #         df['bedrooms'].isin(range(1, 15)) &
-    #         df['bathrooms'].isin(np.arange(1, 5, 0.25)) &  # todo maybe less bathrooms!
-    #         df['floors'].isin(np.arange(1, 4, 0.5)) &
-    #         df['view'].isin(range(0, 5)) &
-    #         df['grade'].isin(range(1, 14)) &
-    #         df['waterfront'].isin([0, 1])]
-    #
-    # df = df.dropna().drop_duplicates()
-    # df['zipcode'] = df['zipcode'].astype(int)
-    # df = df[df['zipcode'].isin(range(98001, 98289))]
-    # df = pd.get_dummies(df, prefidf='zipcode', columns=['zipcode'])
-    # df['yr_built'] = df['yr_built'].astype(int)
-    #
-    # df['yr_renovated'] = np.where(df['yr_renovated'] < 1985, 0, df['yr_renovated'])
-    #
-    # df['yr_built'] = (df['yr_built'] // 10)
-    # df = pd.get_dummies(df, prefidf='yr_built', columns=['yr_built'])
 
 
-def produce_days_before_cancelling_feature(df):
-    df["cancellation_datetime"] = pd.to_datetime(df["cancellation_datetime"])
-    df["checkin_date"] = pd.to_datetime(df["checkin_date"])
-    df["days_before_cancelled"] = df["checkin_date"] - df["cancellation_datetime"]
-    df["days_before_cancelled"] = df["days_before_cancelled"].fillna(pd.Timedelta(0)).dt.days.astype(int)
+def load_train_data_task1(with_dummies):
+    DATA_PATH = "./hackathon_code/data/train.csv"
+    return preprocess_train_task1(DATA_PATH, with_dummies)
 
 
-load_data()
+def load_validation_data_task1():
+    path = "./hackathon_code/data/validation.csv"
+    return preprocess_data_to_validation_task1(path)
+
+
+def load_train_agoda_data_task1(with_dummies):
+    DATA_PATH = "./hackathon_code/agoda_data/agoda_cancellation_train.csv"
+    return preprocess_train_task1(DATA_PATH, with_dummies)
+
+
+def load_test_agoda_data_task1():
+    path = "./hackathon_code/agoda_data/Agoda_Test_1.csv"
+    return preprocess_predict_task1(path)
